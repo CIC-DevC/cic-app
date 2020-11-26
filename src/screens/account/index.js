eaee867e12419a7d97d22cf5e7ab4be3117a1bf7
quan.buivan <quan.buivan@vti.com.vn>
2020-11-26 10:25:19 +0700

    update

--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -151,7 +151,7 @@ const styles = StyleSheet.create({
     borderColor: colors.primary,
     borderWidth: 5,
     alignSelf: 'center',
-    marginTop: 30,
+    marginTop: 60,
   },
   usernameLabel: {
     textAlign: 'center',
